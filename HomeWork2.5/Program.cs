84341ac1e61b9e2d0fa38457a73133126beb6dc5
Malik <poseydon100500@gmail.com>
2023-07-09 22:11:55 +0500

    Добавьте файлы проекта.

---
+++ HomeWork2.5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class HomeWork
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение переменной x : ");
+        float x = float.Parse(Console.ReadLine());
+        float result = (x * x * x / 3)  + (x * x * x * x * x / 5);
+        Console.WriteLine("Результат : " + result);
+        Console.ReadKey();
+    }
+}
